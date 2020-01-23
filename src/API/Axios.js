1f94c6933ffcbb1589184943b7302ef9bd9e7d82
ravi.teja <ravi.koduru@opsucess.com>
2020-01-23 12:35:47 +0530

    Base url change

--- src/API/Axios.js
+++ src/API/Axios.js
@@ -5,8 +5,8 @@ export default axios.create ({
    // const clientId = 'yThM4u19zo-0kLj0SGJCfg'
    // const APIKEY = 'TkLEz6h13EWkDFRVrLojZZWehE5m_n6cohA_Qv72mXNZvOJ_0H5nDAAllyuQOLaCYqEuOE5HYmWi8GdMfSEko4Tu8RRtL2zqeCqnNMZu_hBIKn96rW2mC1tUmyQoXnYx'
 
-   baseURL : 'https://www.yelp.com/developers/documentation/v3/business',
+   baseURL : 'https://api.yelp.com/v3/businesses',
    headers : {
-    Authorization : 'Bearer l2cmhf2ezRl6ZkcHWNYKoDpiaro1zlqUjwkA7nVxnWzryiTwFDk35PJiucoLhjjFY9ECD8GTBGaHBg5yv5YDLiszKQx8EMvm30ply0UWoHQOnYFjLozpYnZOx-UsXXYx'
+    Authorization : 'Bearer TkLEz6h13EWkDFRVrLojZZWehE5m_n6cohA_Qv72mXNZvOJ_0H5nDAAllyuQOLaCYqEuOE5HYmWi8GdMfSEko4Tu8RRtL2zqeCqnNMZu_hBIKn96rW2mC1tUmyQoXnYx'
    }
 });
